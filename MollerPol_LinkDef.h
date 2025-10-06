4891a104253064a261bb6506fb12567d075e038c
hatdaq <hatdaq@comptondaq.jlab.org>
2025-10-06 16:32:23 -0400

    add scaler decoding, specifically the fadc scaler decoding

--- MollerPol_LinkDef.h
+++ MollerPol_LinkDef.h
@@ -6,5 +6,7 @@
 
 #pragma link C++ class MollerPolCalorimeter+;
 #pragma link C++ class MollerPolApparatus+;
+#pragma link C++ class MollerPolScalerEvtHandler+;
+#pragma link C++ class Decoder::Scaler9250+;
 
 #endif
